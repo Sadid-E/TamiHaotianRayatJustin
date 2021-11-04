b966b4d4c5692cd1cade698b7594cb7ae6c9f615
Tami Takada <ttakada20@stuy.edu>
2021-11-04 08:39:35 -0400

    fixed write, added create_user

--- app/database.py
+++ app/database.py
@@ -10,7 +10,7 @@ def create_tables():
     command = 'CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY, title TEXT, user_id INTEGER KEY)'          # test SQL stmt in sqlite3 shell, save as string
     c.execute(command)    # run SQL statement
 
-    command = 'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, password TEXT, entry_ids TEXT)'          # test SQL stmt in sqlite3 shell, save as string
+    command = 'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, password TEXT)'          # test SQL stmt in sqlite3 shell, save as string
     c.execute(command)
 
     db.commit() #save changes
@@ -29,7 +29,7 @@ def write_entry_to_db(title, entry_text, entry_id, user_id):
         if len(ids) == 0:
             id = 0
         else:
-            id = ids[0]
+            id = ids[0][0] + 1
 
     file = open(f'{id}.txt', 'w')
     file.write(entry_text)
@@ -39,4 +39,24 @@ def write_entry_to_db(title, entry_text, entry_id, user_id):
     c.execute(command)
 
     db.commit() #save changes
-    db.close() 
+    db.close()
+
+def create_user(name, password):
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+
+    command = 'SELECT id FROM users ORDER BY id DESC LIMIT 1'
+    c.execute(command)
+
+    id = 0
+    ids = c.fetchall()
+    if len(ids) > 0:
+        print(ids)
+        id = ids[0][0] + 1
+    print(id)
+
+    command = f'INSERT INTO users VALUES ({id}, "{name}", "{password}");'
+    c.execute(command)
+
+    db.commit() #save changes
+    db.close()
