e3172b7912916b283532d701ccd864810f2b6362
BuildTools <unconfigured@null.spigotmc.org>
2021-11-06 07:04:15 -0400

    Updates

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from sqlite3.dbapi2 import IntegrityError
 from flask import Flask, render_template, request, session, redirect
 import os
 import database
@@ -5,15 +6,32 @@ import database
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
-
-
 @app.route('/', methods=['GET', 'POST'])
 def display_login():
-    database.create_tables()
+    if(session.get("user_id") == None):
+        return redirect("/login") 
+    return render_template(
+        'home.html', user_id=session.get("user_id"), 
+    )
 
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.clear()
+    return redirect('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if(session.get("user_id")):
+        return redirect("/")
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user_id = database.authenticate(username, password)
+        if(user_id):
+            session["user_id"] = user_id #Cookie based authentication (user is identified by his client id)
+            return redirect("/")
+        else:
+            return render_template('login.html', error=True)
 
     return render_template(
         'login.html'
@@ -21,52 +39,99 @@ def display_login():
 
 @app.route('/register', methods=['GET','POST'])
 def register_user():
+    if(session.get("user_id")):
+        return redirect("/")
     password = ''
     confirm = ''
-    incorrect = False
     if(request.method == 'POST'):
         username = request.form['username']
         password = request.form['password']
         confirm = request.form['confirm']
         if(str(password) != str(confirm) or password == '' or confirm == ''):
-            print("hi")
-            return render_template(
-                'register.html',
-                incorrect = True
-                )
+            return render_template('register.html', error = True, 
+                error_message="Your passwords didn't match :(")
         elif(str(password) == str(confirm)):
-            print('test')
-            database.create_user(username, password)
-    print(incorrect)
+            try:
+                session['user_id'] = database.create_user(username, password)
+                return redirect("/")
+            except IntegrityError:
+                return render_template('register.html', error = True, 
+                error_message="That username is already taken. Please pick another one.")
+            except Exception:
+                return render_template('register.html', error = True, 
+                error_message="Sorry, something went wrong on our end. Please try registering later.")
+    
     return render_template(
-        'register.html',
-        incorrect = False
+        'register.html'
     )
-@app.route('/blog/<user_id>', methods=['GET', 'POST'])
+@app.route('/blog/<int:user_id>', methods=['GET', 'POST'])
 def display_user_blog(user_id):
+    
+    templateArgs = {
+        "entries" : database.get_entries_of_user(user_id, 0, 50),
+        "username" : database.get_username_from_id(user_id),
+        "lookingAtOwnBlog": session.get("user_id") == user_id
+    }
     return render_template(
         'blog.html',
-        username = 'Test Username',
-        entries = [{'name': 'Name 0', 'text': 'Test text', 'url': '/entry/0'}, {'name': 'Name 1', 'text': 'stuff', 'url': '/entry/1'}]
+        **templateArgs
     )
 
-@app.route('/entry/<blog_entry_id>', methods=['GET', 'POST'])
-def display_entry(blog_entry_id):
+@app.route('/entry/<int:entry_id>', methods=['GET', 'POST'])
+def display_entry(entry_id):
+    template_args = database.get_entry(entry_id) 
+    template_args["username"] = database.get_username_from_id(template_args["user_id"])
+    template_args["original_author"] = session.get("user_id") == template_args["user_id"]
+    print(session.get("user_id"))
     return render_template(
         'entry.html',
-        entry_name = 'Test Name',
-        entry_user = 'Test User',
-        full_entry = ['Line 0', 'Line 1', 'Line 2']
+        **template_args
     )
 
-@app.route('/entry/<blog_entry_id>/edit', methods=['GET', 'POST'])
-def display_entry_edit(blog_entry_id):
+@app.route('/blog/newBlogEntry', methods=['GET', 'POST'])
+def create_new_entry():
+    if(session.get("user_id") == None):
+        return redirect("/")
+    if(request.method == 'POST'):
+        user_id = session.get("user_id")
+        new_entry_text = request.form.get('entry_text')
+        new_title = request.form.get('title')
+        database.add_entry(new_title, new_entry_text, user_id)
+
+        #This assumes that the entry_id of the one we just added to the database, is the user's most recent entry
+        assumed_entry_id = database.getMostRecentEntry(user_id)["entry_id"] 
+        return redirect(f"/entry/{assumed_entry_id}")
+    return render_template("newBlogEntry.html", user_id=session.get("user_id"))
+
+@app.route('/entry/<int:entry_id>/edit', methods=['GET', 'POST'])
+def display_entry_edit(entry_id):
+    if(session.get("user_id") == None):
+        return redirect("/")
     if request.method == 'POST':
-        text = request.form['entry']
-        # write_entry_to_db(text, request.args['blog_entry_id'])
-        return redirect('/entry/' + blog_entry_id)
-    # return render_template('entry_edit.html', text = get_entry_from_db(request.args['blog_entry_id']))
-    return 'Hello'
+        author_id = database.get_entry(entry_id)["user_id"]
+        if(not author_id == session.get("user_id")):
+            return "Sorry, you can't modify this blog post if you aren't its author."
+        new_entry_text = request.form.get('entry_text')
+        new_title = request.form.get('title')
+        database.edit_entry(entry_id, new_entry_text, new_title)
+        return redirect(f"/entry/{entry_id}")
+    if request.method == 'GET':
+        templateArgs = database.get_entry(entry_id)
+        return render_template('entry_edit.html', **templateArgs)    
+
+@app.route('/entry/delete', methods=['POST'])
+def delete():
+    if(session.get("user_id") == None):
+        return redirect("/")
+    entry_id = request.form["entry_id"]
+    author_id = database.get_entry(entry_id)["user_id"]
+    user_id = session.get("user_id")
+    if(not author_id == user_id):
+        return "Sorry, you can't modify this blog post if you aren't its author."
+
+    database.delete_entry(entry_id)
+    return redirect(f"/blog/{user_id}")
+    
 
 if __name__ == '__main__':
     app.debug = True
--- app/database.py
+++ app/database.py
@@ -1,62 +1,94 @@
 import sqlite3   #enable control of an sqlite database
-import csv       #facilitate CSV I/O
 
 DB_FILE='database.db'
+db = sqlite3.connect(DB_FILE, check_same_thread=False)
+
 
 def create_tables():
-    db = sqlite3.connect(DB_FILE)
-    c = db.cursor()
 
-    command = 'CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY, title TEXT, user_id INTEGER KEY)'          # test SQL stmt in sqlite3 shell, save as string
+    c = db.cursor()
+    command = 'CREATE TABLE IF NOT EXISTS entries (entry_id INTEGER PRIMARY KEY, title TEXT, entry_text TEXT, user_id INTEGER)'          # test SQL stmt in sqlite3 shell, save as string
     c.execute(command)    # run SQL statement
 
-    command = 'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, password TEXT)'          # test SQL stmt in sqlite3 shell, save as string
+    command = 'CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, username TEXT NOT NULL UNIQUE, password TEXT NOT NULL)'          # test SQL stmt in sqlite3 shell, save as string
     c.execute(command)
 
     db.commit() #save changes
-    db.close()  #close database
 
-def write_entry_to_db(title, entry_text, entry_id, user_id):
-    db = sqlite3.connect(DB_FILE)
+def create_user(username, password):
     c = db.cursor()
-
-    id = entry_id
-    if entry_id == -1:
-        command = 'SELECT id FROM entries ORDER BY id DESC LIMIT 1'
-        c.execute(command)
-
-        ids = c.fetchall()
-        if len(ids) == 0:
-            id = 0
-        else:
-            id = ids[0][0] + 1
-
-    file = open(f'{id}.txt', 'w')
-    file.write(entry_text)
-    file.close()
-
-    command = f'INSERT INTO entries VALUES ({id}, "{title}", {user_id});'
-    c.execute(command)
-
-    db.commit() #save changes
-    db.close()
-
-def create_user(name, password):
-    db = sqlite3.connect(DB_FILE)
+    c.execute(f'INSERT INTO users (username, password) VALUES (?, ?);', (username, password))
+    db.commit() 
+    
+def add_entry(title, entry_text, user_id): 
     c = db.cursor()
-
-    command = 'SELECT id FROM users ORDER BY id DESC LIMIT 1'
-    c.execute(command)
-
-    id = 0
-    ids = c.fetchall()
-    if len(ids) > 0:
-        print(ids)
-        id = ids[0][0] + 1
-    print(id)
-
-    command = f'INSERT INTO users VALUES ({id}, "{name}", "{password}");'
-    c.execute(command)
-
+    c.execute(f'INSERT INTO entries (title, entry_text, user_id) VALUES (?, ?, ?)', (title, entry_text, user_id))
     db.commit() #save changes
-    db.close()
+
+def edit_entry(entry_id, entry_text, title):
+    c = db.cursor()
+    c.execute(f'UPDATE entries SET entry_text = ?, title = ? where entry_id == ?', (entry_text, title, entry_id))
+    db.commit()
+    return
+
+def delete_entry(entry_id):
+    c = db.cursor()
+    c.execute(f'delete from entries where entry_id == ?', (entry_id, ))
+    db.commit()
+    return
+
+def get_entry(entry_id):
+    c = db.cursor()
+    result = list(c.execute(f'select entry_id, entry_text, title, user_id from entries where entry_id == ?', (entry_id, )))
+    if(len(result) == 0):
+        return None
+    return [{
+        "entry_id": entry_id,
+        "entry_text": entry_text,
+        "title": title,
+        "user_id": user_id
+    } for (entry_id, entry_text, title, user_id) in result][0]
+
+def get_entries_of_user(user_id, offset, limit):
+    c = db.cursor()
+    result = list(c.execute(f'select entry_id, entry_text, title, user_id from entries where user_id == ? order by entry_id limit ? offset ? ', (user_id, limit, offset)))
+    return [{
+        "entry_id": entry_id,
+        "entry_text": entry_text,
+        "title": title,
+        "user_id": user_id
+    } for (entry_id, entry_text, title, user_id) in result]
+def authenticate(username, password):
+    c = db.cursor()
+    result = list(c.execute(f'SELECT user_id from users where username == ? and password == ?', (username, password)))
+    if(len(result) == 0): #length 0 means that password/username combination had no match
+        return None
+    return result[0][0] #user_id
+
+def get_username_from_id(user_id):
+    c = db.cursor()
+    result = list(c.execute(f'SELECT username from users where user_id == ?', (user_id, )))[0][0]
+    print(result)
+    return result
+
+def getMostRecentEntry(user_id):
+    c = db.cursor()
+    result = list(c.execute(f'select entry_id, entry_text, title, user_id from entries where user_id == ? order by entry_id DESC limit 1', (user_id, )))
+    return [{
+        "entry_id": entry_id,
+        "entry_text": entry_text,
+        "title": title,
+        "user_id": user_id
+    } for (entry_id, entry_text, title, user_id) in result][0]
+
+def printEverything():
+    print(list(db.cursor().execute("select * from entries")))
+
+
+
+
+
+
+
+
+
