86cb944b4e8a8c068e1814e4925cca7def0c5e33
BuildTools <unconfigured@null.spigotmc.org>
2021-11-08 08:33:37 -0500

    Tables should be created once at start

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,6 @@ database.create_tables()
 
 @app.route('/', methods=['GET', 'POST'])
 def display_login():
-    database.create_tables()
     if(session.get("user_id") == None):
         return redirect("/login")
     return render_template(
